ce1fa00793058e4c216469ce7a15d59d1515edd6
Rafał Lach <rafal.lach86@gmail.com>
2019-09-06 13:57:50 +0200

    Support game in multiple folders in findDuplicates, detect broken version.txt files (#17)

--- src/scripts/find_possible_duplicates.js
+++ src/scripts/find_possible_duplicates.js
@@ -1,9 +1,9 @@
-const log = require('../util/logger');
+const Hjson = require('hjson');
 const fs = require('fs');
 
 function findPossibleDuplicates(mainPaths) {
     const foundFiles = [];
-    const duplicates = {};
+    const duplicates = [];
     for (const path of mainPaths) {
         const singlePathFiles = fs.readdirSync(path).map(name => {
             return {
@@ -15,8 +15,11 @@ function findPossibleDuplicates(mainPaths) {
     }
 
     for (const file of foundFiles) {
-        const subdirectories = fs
-            .readdirSync(`${file.path}/${file.name}`, { withFileTypes: true })
+        const allSubFiles = fs.readdirSync(`${file.path}/${file.name}`, {
+            withFileTypes: true,
+        });
+
+        const subdirectories = allSubFiles
             .filter(dirent => dirent.isDirectory())
             .map(dirent => dirent.name);
 
@@ -32,26 +35,93 @@ function findPossibleDuplicates(mainPaths) {
                 .split('\n')
                 .map(v => v.trim());
 
-            const acceptedSubdirectories = subdirectories.filter(d =>
-                acceptedVersions.includes(d)
-            );
-            if (acceptedSubdirectories.length > 0) {
-                versions = versions - acceptedSubdirectories.length + 1; // Add 1 because all accepted versions count as 1
+            if (versions < acceptedVersions.length) {
+                versions = -2;
+            } else {
+                const acceptedSubdirectories = subdirectories.filter(d =>
+                    acceptedVersions.includes(d)
+                );
+                if (acceptedSubdirectories.length > 0) {
+                    versions = versions - acceptedSubdirectories.length + 1; // Add 1 because all accepted versions count as 1
+                }
             }
         }
 
-        if (versions > 1) {
-            log.debug(
-                `${file.name} contains ${subdirectories.length -
-                    1} possible duplicate(s)`
-            );
-            duplicates[file.name] = subdirectories.length - 1;
-        } else if (subdirectories.length === 0) {
-            duplicates[file.name] = -1;
+        if (versions === 0 && allSubFiles.length > 0) {
+            versions = -1;
         }
+
+        addResult(duplicates, file.name, versions, file.path);
     }
 
-    fs.writeFileSync('duplicates.txt', JSON.stringify(duplicates, null, 4));
+    let results = duplicates
+        .filter(d => d.sum > 1 || d.hasWrongEntries || d.hasWrongVersionTxt)
+        .sort((a, b) => b.sum - a.sum)
+        .reduce((target, current) => {
+            let result = {};
+
+            let statuses = [];
+            if (current.sum > 1) {
+                statuses.push(`${current.sum} duplicate(s)`);
+            }
+            if (current.hasWrongEntries) {
+                statuses.push('has wrong structure');
+            }
+            if (current.hasWrongVersionTxt) {
+                statuses.push('has wrong version.txt');
+            }
+            result.status = statuses.join(', ');
+            current.values.forEach(v => {
+                result[`${v.path}/${current.name}`] = nameFromValue(v);
+            });
+
+            target[current.name] = result;
+            return target;
+        }, {});
+
+    fs.writeFileSync(
+        'duplicates.txt',
+        Hjson.stringify(results, {
+            bracesSameLine: true,
+        })
+    );
+}
+
+function nameFromValue(value) {
+    if (value.value === -2) {
+        return 'wrong version.txt';
+    } else if (value.value === -1) {
+        return 'wrong structure';
+    } else {
+        return `${value.value} duplicate(s)`;
+    }
+}
+
+function addResult(results, name, value, path) {
+    let result = results.find(r => r.name === name);
+
+    let entry = {
+        value,
+        path,
+    };
+
+    if (!result) {
+        result = {
+            name,
+            values: [entry],
+            sum: Math.max(value, 0),
+            hasWrongEntries: value === -1,
+            hasWrongVersionTxt: value === -2,
+        };
+
+        results.push(result);
+    } else {
+        result.values.push(entry);
+        result.sum += Math.max(value, 0);
+        result.hasWrongEntries = results.hasWrongEntries || value === -1;
+        result.hasWrongVersionTxt =
+            result.hasWrongVersionTxt || result.hasWrongVersionTxt === -2;
+    }
 }
 
 module.exports = findPossibleDuplicates;
